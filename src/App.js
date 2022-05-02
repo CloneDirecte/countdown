9a5c887bb70480aa0c2de18208de654b35f7e7e0
Hugo <24863815+bgpvp@users.noreply.github.com>
2022-05-02 22:56:19 +0200

    chore: init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [dateArretTravail, setDateArretTravail] = useState([]);
+  const [dateVacances, setDateVacances] = useState([]);
+  useEffect(() => {
+    var today = new Date().getTime();
+    var oneDay = 24 * 60 * 60 * 1000;
+    var arretTravailIntense = new Date("05/26/2022").getTime();
+    var vacances = new Date("06/11/2022").getTime();
+    setDateArretTravail(
+      Math.round(Math.abs((arretTravailIntense - today) / oneDay))
+    );
+    setDateVacances(Math.round(Math.abs((vacances - today) / oneDay)));
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="arret">
+        <h1>Jours avant dernier DS</h1>
+        <div className="numbers">
+          {Array.from(String(dateArretTravail), Number).map((number) => (
+            <div className="number" key={number}>
+              <h1 key={number}>{number}</h1>
+            </div>
+          ))}
+          <h1>jours</h1>
+        </div>
+      </div>
+      <div className="arret">
+        <h1>Jours avant vacances</h1>
+        <div className="numbers">
+          {Array.from(String(dateVacances), Number).map((number) => (
+            <div className="number" key={number}>
+              <h1 key={number}>{number}</h1>
+            </div>
+          ))}
+          <h1>jours</h1>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
